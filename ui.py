bb8b26be6c335fda4bb09aefdd4a79dde9b13bf9
zhongcheng zhao <zhongcheng9114@gmail.com>
2025-03-02 16:55:08 -0600

    completed user ui

--- ui.py
+++ ui.py
@@ -8,9 +8,14 @@ class QuizInterface:
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR,padx=20,pady=20)
-        self.canvas = Canvas(width=300,height=250,bg="white",highlightthickness=0)
-        self.text = self.canvas.create_text(150,120,text= "text",font=FONT)
-        self.canvas.grid(row=1,column=0,columnspan=2)
+
+        self.score_label =Label(text="Score: 0",fg="white",bg=THEME_COLOR)
+        self.score_label.grid(row=0,column=1)
+
+        self.canvas = Canvas(width=300,height=250,bg="white")
+        self.text = self.canvas.create_text(150,120,text= "text",font=FONT,fill=THEME_COLOR)
+        self.canvas.grid(row=1,column=0,columnspan=2,pady=50)
+
         self.true_image = PhotoImage(file="images/true.png")
         self.false_image = PhotoImage(file="images/false.png")
         self.true_button = Button(image=self.true_image,highlightthickness=0)
